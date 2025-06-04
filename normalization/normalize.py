551a669f8bd93f8042cfb18dd769641bd47d285c
Tyler Carver <tdc266@nau.edu>
2025-06-04 09:08:45 -0700

    test commit

--- normalization/normalize.py
+++ normalization/normalize.py
@@ -123,3 +123,5 @@ def full_pipeline(
     #    out_moving_mask_aligned = "output/topcow_ct_001_0000/nn_space/topcow_001_CTmask_inMRspace.nii.gz",
     #    transform_prefix = "topcow-001_CT2MR_"
     #)
+
+    # test
