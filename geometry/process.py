62a413f4aa87dd666dda3cf8b19cc79d867de984
Conrad765 <cas2349@nau.edu>
2025-06-12 13:32:28 -0700

    process file

---
+++ geometry/process.py
@@ -0,0 +1,45 @@
+import argparse
+import logging
+import os
+import sys
+from os.path import exists, join
+import shutil
+from pipeline import pipeline
+
+def parse_arguments() -> argparse.Namespace:
+    """Simple CommandLine argument parsing function making use of the argsparse module.
+
+    Returns:
+        argparse.Namespace: command line arguments.
+    """
+
+    parser = argparse.ArgumentParser(
+        prog="eICAB",
+        description="Vascular Medical Image Automatic Analysis. "
+        "Optimize for CW analysis.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "input_folder",
+        help="ToF MRA image [ 3D image | .nii/ .nii.gz ].",
+    )
+    parser.add_argument(
+        "patient_ID",
+        help="patient ID"
+    )
+    parser.add_argument(
+        "output",
+        help="Defines the output folder.",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = parse_arguments
+    pipeline(args, prefix="topcow")
+    
+
+if __name__ == "__main__":
+    main()
