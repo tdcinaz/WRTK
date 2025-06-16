3df163a0c6b4c18c4d7de695025815e10c29ffa4
Conrad765 <cas2349@nau.edu>
2025-06-16 09:11:44 -0700

    diameters preprocess

---
+++ geometry/diameters_preprocess.py
@@ -0,0 +1,46 @@
+import argparse
+import logging
+import os
+import sys
+from os.path import exists, join
+import shutil
+from pipeline import pipeline
+
+def parse_arguments() -> argparse.Namespace:
+    """Simple CommandLine argument parsing function making use of the argsparse module.
+
+    Returns:
+        argparse.Namespace: command line arguments.
+    """
+
+    parser = argparse.ArgumentParser(
+        prog="eICAB",
+        description="Vascular Medical Image Automatic Analysis. "
+        "Optimize for CW analysis.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "input_folder",
+        help="Extracted Scan",
+    )
+    parser.add_argument(
+        "patient_ID",
+        help="patient ID"
+    )
+    parser.add_argument(
+        "output",
+        help="Defines the output folder.",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = parse_arguments()
+    logging.info(f"Patient ID: {args.patient_ID}")
+    pipeline(args, prefix="topcow")
+    
+
+if __name__ == "__main__":
+    main()
