d2b1eaad1807d343c0816ed7df9a7ff782cf55d0
tdcinaz <tdc266@nau.edu>
2025-06-16 09:11:47 -0700

    Started angle extraction pipeline

---
+++ geometry/angles.py
@@ -0,0 +1,25 @@
+# 1. Apply Vessel Enhancing Diffusion
+#--------Use eICAB filter from ComputeVED
+
+# 2. Skeletonize complete artery network
+
+# 3. Find largest radius spheres
+
+# 4. Label centerline voxels based on parent arteries
+
+# 5. Label start and end voxels
+#--------Use SurfaceNets3D to identify connection points
+
+# 6. Isolate individual arteries
+
+# 7. Detect and trim loops
+
+# 8. Path finding
+
+# 9. Curve fitment
+
+# 10. Network reassembly
+
+# 11. Angle Calculation
+
+# 12. Data export
